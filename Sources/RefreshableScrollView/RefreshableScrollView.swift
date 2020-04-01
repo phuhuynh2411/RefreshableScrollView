b72cc8a59f44b8ecd53f09db6bc36b34a4d253f3
Phu Huynh <phu@filesoft.tech>
2020-04-01 22:54:46 +0700

    remove offset

--- Sources/RefreshableScrollView/RefreshableScrollView.swift
+++ Sources/RefreshableScrollView/RefreshableScrollView.swift
@@ -67,8 +67,6 @@ public struct RefreshableScrollView<Content: View>: View {
                             self.content
                         }
                         .alignmentGuide(.top, computeValue: { d in (self.refreshing && self.frozen) ? -self.threshold : 0.0 })
-                    .offset(x: 0, y: 80)
-                        
                         .onPreferenceChange(RefreshableKeyTypes.PrefKey.self) { values in
                             self.refreshLogic(values: values)
                         }
