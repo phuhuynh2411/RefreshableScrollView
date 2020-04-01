466059ecd95b7da71148446e8916eafb7d02bd9f
Phu Huynh <phu@filesoft.tech>
2020-04-01 23:36:57 +0700

    a

--- Sources/RefreshableScrollView/RefreshableScrollView.swift
+++ Sources/RefreshableScrollView/RefreshableScrollView.swift
@@ -62,20 +62,18 @@ public struct RefreshableScrollView<Content: View>: View {
             } else {
                 VStack{
                     ZStack(alignment: .top) {
-                        SymbolView(height: self.threshold, loading: self.refreshing, frozen: self.frozen, rotation: self.rotation, activityView: self.activityView, pullView: self.pullView)
-                        VStack {
-                            List {
-                                self.content
+                        List {
+                            self.content
                                 .background(MovingView())
-                            }
-                            .offset(x: 0, y: (self.refreshing && self.frozen) ? -self.threshold : 0.0)
-                            .background(FixedView())
+                        }
+                        .offset(x: 0, y: (self.refreshing && self.frozen) ? -self.threshold : 0.0)
+                        .background(FixedView())
                             //.alignmentGuide(.top, computeValue: { d in (self.refreshing && self.frozen) ? -self.threshold : 0.0 })
                             
                             .onPreferenceChange(RefreshableKeyTypes.PrefKey.self) { values in
                                 self.refreshLogic(values: values)
-                            }
                         }
+                        SymbolView(height: self.threshold, loading: self.refreshing, frozen: self.frozen, rotation: self.rotation, activityView: self.activityView, pullView: self.pullView)
                     }
                 }
                 
