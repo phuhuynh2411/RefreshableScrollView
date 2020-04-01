f05a940bd6e1cd5d9802b22572f4313d90b00829
Phu Huynh <phu@filesoft.tech>
2020-04-01 23:08:43 +0700

    aa

--- Sources/RefreshableScrollView/RefreshableScrollView.swift
+++ Sources/RefreshableScrollView/RefreshableScrollView.swift
@@ -63,8 +63,9 @@ public struct RefreshableScrollView<Content: View>: View {
                 VStack{
                     ZStack(alignment: .top) {
                         List {
-                            MovingView()
+                            
                             self.content
+                            .listRowBackground(MovingView())
                         }
                         .alignmentGuide(.top, computeValue: { d in (self.refreshing && self.frozen) ? -self.threshold : 0.0 })
                         .onPreferenceChange(RefreshableKeyTypes.PrefKey.self) { values in
